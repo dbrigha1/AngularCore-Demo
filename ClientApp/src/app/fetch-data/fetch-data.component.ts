d361a747410fb87f43de3da97a8878d3154db400
Dylan Brigham <dbrigha1@uwyo.edu>
2018-07-09 18:03:36 -0600

    refactored fetch-data.component

--- ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -1,12 +1,13 @@
-import { Component, Inject } from '@angular/core';
-import { HttpClient, HttpSentEvent } from '@angular/common/http';
+import { Component, Inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-fetch-data',
   templateUrl: './fetch-data.component.html'
 })
 
-export class FetchDataComponent {
+export class FetchDataComponent implements OnInit {
+  
     public persons: PersonData[];
     public readonly _http: HttpClient;
     public readonly _baseUrl:string;
@@ -14,6 +15,9 @@ export class FetchDataComponent {
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
     this._http = http;
     this._baseUrl=baseUrl;
+  }
+
+  ngOnInit(): void {
     this.GetAll();
   }
 
@@ -21,14 +25,13 @@ export class FetchDataComponent {
   {
     this._http.get<PersonData[]>(this._baseUrl + 'api/Persons/Person').subscribe(result => {
     this.persons = result;
-    }, error => console.error(error));   
+    }, error => console.error(error));
   }
 
   public DeleteAll() :void
   {
     console.log("Deleting Persons...");
-    this._http.delete(this._baseUrl+'api/Persons/PersonDeleteAll').subscribe(persons=>this.persons);
-    this.persons = null;
+    this._http.delete<PersonData[]>(this._baseUrl+'api/Persons/PersonDeleteAll').subscribe(result=> {this.persons = result;});
   };
 
 }
